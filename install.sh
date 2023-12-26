d9d87c0c5d37a472808fd26867acd41db612b7f7
pgibler <paul.gibler@gmail.com>
2023-12-26 03:57:55 -0500

    Replaced alias with symlink executable in bin

--- install.sh
+++ install.sh
@@ -20,19 +20,15 @@ install_node_arch() {
 install_node_redhat() {
     echo "Installing Node.js and npm..."
     sudo yum install nodejs npm xsel -y
-
     echo "Node.js and npm have been installed."
 }
 
 # Detect the package manager and install Node.js
 if command -v apt > /dev/null; then
-    # Debian or Ubuntu
     install_node_ubuntu
 elif command -v pacman > /dev/null; then
-    # Arch Linux
     install_node_arch
 elif command -v yum > /dev/null; then
-    # Red Hat-based systems
     install_node_redhat
 else
     echo "Unsupported package manager or Linux distribution."
@@ -43,50 +39,26 @@ node -v
 npm --version
 
 # Check if npm is available
-if ! command -v npm &> /dev/null
-then
+if ! command -v npm &> /dev/null; then
     echo "npm could not be found, please install Node.js and npm"
     exit 1
 fi
 
-# Check if node is available
+# Install npm dependencies
 npm install
 
 # Setup .env file
 node ./src/run.mjs configure
 
-#!/bin/bash
-
 # Get the directory where the install script is located
 CMDH_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-# Alias command using the dynamically determined path
-alias_cmd="alias cmdh='node $CMDH_DIR/src/run.mjs'"
+# Create a run.sh script
+echo "#!/bin/bash" > "$CMDH_DIR/run.sh"
+echo "node $CMDH_DIR/src/run.mjs \"\$@\"" >> "$CMDH_DIR/run.sh"
+chmod +x "$CMDH_DIR/run.sh"
 
-# Function to add alias and reload config for Bash and Zsh
-add_alias_and_reload() {
-    shell_rc=$1
-    echo "$alias_cmd" >> "$shell_rc"
-    source "$shell_rc"
-}
-
-# Detect the shell and update the corresponding config file
-if [ -n "$BASH_VERSION" ]; then
-    # Bash shell
-    add_alias_and_reload "${HOME}/.bashrc"
-    echo "Alias added to .bashrc and reloaded."
-elif [ -n "$ZSH_VERSION" ]; then
-    # Zsh shell
-    add_alias_and_reload "${HOME}/.zshrc"
-    echo "Alias added to .zshrc and reloaded."
-elif [ -n "$FISH_VERSION" ]; then
-    # Fish shell
-    echo "set -Ux cmdh 'node $CMDH_DIR/src/run.mjs'" | fish
-    echo "Alias added to Fish universal variables."
-else
-    echo "Unsupported shell. Please add the alias manually to your shell initializer."
-    echo "$alias_cmd"
-    exit 1
-fi
+# Create a symlink to the run.sh script in /usr/local/bin
+sudo ln -sf "$CMDH_DIR/run.sh" /usr/local/bin/cmdh
 
 echo "Installation complete! You can now use the 'cmdh' command."
