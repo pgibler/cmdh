75f9986b899bd9ea4d6c22f424fe3df7e6fd009b
pgibler <paul.gibler@gmail.com>
2023-12-25 20:51:20 -0500

    Simplified install step

--- install.sh
+++ install.sh
@@ -18,7 +18,19 @@ fi
 npm install
 
 # Ask user to run update_path.sh
-read -p "Do you want to update the PATH variable? (yes/no): " choice
-if [ "$choice" = "yes" ]; then
-    ./update_path.sh
+read -p "Do you want to update the PATH variable? (Y/n): " choice
+if [ "$choice" = "Y" -o "$choice" = "y" -o "$choice" = "yes" ]; then
+    # Set up the alias in .bashrc or .zshrc
+    if [ -n "$BASH_VERSION" ]; then
+      echo "alias cmdh='node ~/cmdh/src/run.mjs'" >> ~/.bashrc
+      source ~/.bashrc
+    elif [ -n "$ZSH_VERSION" ]; then
+      echo "alias cmdh='node ~/cmdh/src/run.mjs'" >> ~/.zshrc
+      source ~/.zshrc
+    else
+      echo "Unsupported shell. Please add the alias manually."
+      exit 1
+    fi
 fi
+
+echo "Installation complete! You can now use the 'cmdh' command."
