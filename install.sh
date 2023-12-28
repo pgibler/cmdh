d0cd6a3e2254c5d0a91c56ba3f3019b293db1f7f
pgibler <paul.gibler@gmail.com>
2023-12-28 06:53:40 -0500

    Removed autoinstall of node / npm

--- install.sh
+++ install.sh
@@ -1,48 +1,18 @@
 #!/bin/bash
 
-# Function to install Node.js, npm, and xsel for clipboardy support.
-install_node_ubuntu() {
-    echo "Running updates..."
-    sudo apt update
-    echo "Installing Node.js and npm..."
-    sudo apt install nodejs npm xsel -y
-    echo "Node.js and npm have been installed."
-}
-
-# Function to install Node.js and npm for Arch Linux
-install_node_arch() {
-    echo "Installing Node.js and npm..."
-    sudo pacman -Sy nodejs npm xsel
-    echo "Node.js and npm have been installed."
-}
-
-# Function to install Node.js and npm for Red Hat-based systems
-install_node_redhat() {
-    echo "Installing Node.js and npm..."
-    sudo yum install nodejs npm xsel -y
-    echo "Node.js and npm have been installed."
-}
-
-# Detect the package manager and install Node.js
-if command -v apt > /dev/null; then
-    install_node_ubuntu
-elif command -v pacman > /dev/null; then
-    install_node_arch
-elif command -v yum > /dev/null; then
-    install_node_redhat
-else
-    echo "Unsupported package manager or Linux distribution."
-    exit 1
-fi
-
-node -v
-npm --version
-
 # Check if npm is available
 if ! command -v npm &> /dev/null; then
     echo "npm could not be found, please install Node.js and npm"
     exit 1
 fi
+
+if ! command -v node &> /dev/null; then
+    echo "node could not be found, please install Node.js and npm"
+    exit 1
+fi
+
+node -v
+npm --version
 
 # Install npm dependencies
 npm install
